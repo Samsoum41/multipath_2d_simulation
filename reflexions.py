fa136c32666cb6989da7472e119e926e61b0ecea
Samsoum41 <hocine.samy.sh@gmail.com>
2021-11-10 23:48:45 +0100

    Deleting geometry functions existing in sympy

--- reflexions.py
+++ reflexions.py
@@ -1,33 +1,16 @@
 import turtle as tu
 from math import sqrt
 from random import random
-from sympy import Point, Line
+from sympy import Point, Line, Segment, Polygon
 
-            
-"""
-    Returns the virtual images of S on the grid as (point,(orientation,position_of_reflection_plane)) pairs
-"""
-def reflexions_possibles(S,grille):
-    # On suppose que la grille est de la forme (-x,x,y-y), donc rectangulaire.
-    x,y=grille[1],grille[2]                                                     
-    xs,ys=S
-    res=[((2*x-xs,ys),('vertical',x)),((-2*x-xs,ys),('vertical',-x)),((xs,2*y-ys),('horizontal',y)),((xs,-2*y-ys),('horizontal',-y))]
-    # We filter edges that are inside the grid, why ? 
-    #TODO : Why ?
-    if xs>=x: res.remove(((2*x-xs,ys),('vertical',x)))
-    if xs<=-x: res.remove(((-2*x-xs,ys),('vertical',-x)))
-    if ys<=-y:res.remove(((xs,-2*y-ys),('horizontal',-y)))
-    if ys>=y:res.remove(((xs,2*y-ys),('horizontal',y)))
-    return res
-    # On peut remarquer que res ne sera jamais vide, elle contient toujours au moins 2 éléments
-
-
-
-def distance(A,B): 
-    ((xa,ya),(xb,yb))=A,B
-    return(sqrt((xa-xb)**2 + (ya-yb)**2))
-
-def multipath(S,A,grille,n,derniere_reflexion=None):
+def segmentInTheGrid(Seg:Segment, grille:Polygon):
+    I = grille.intersection(Seg)
+    #TODO Deal with border cases
+    if I:
+        return [I, Seg.vertices[1]] if S == I else [Seg.vertices[0], I]
+    else : 
+        return [] 
+def multipath(S:Point,A:Point,grille:Polygon,n,derniere_reflexion=None):
     res=[]
     if n==0:
         segmentInterieur=segmentInTheGrid(S,A,grille)
@@ -50,180 +33,23 @@ def multipath(S,A,grille,n,derniere_reflexion=None):
                     departure, arrival = segmentInterieur
                     tracer(departure,arrival)
                     res+=[departure]
-        return res
-"""
-def multipath(S,A,grille,n,derniere_reflexion=None):
-    res=[]
-    if n==0:
-        S=intersection(S,A,derniere_reflexion,grille)
-        if S:
-            tracer(S,A,'red')
-        return S
-    else:                                       
-        # virtualImages contient une liste de couple ( objet virtuel, direction du miroir par rapport auquel les objets sont symétriques)
-        virtualImages=reflexions_possibles(S,grille) 
-        for image in virtualImages:                                        
-            points=multipath(image[0],A,grille,n-1,derniere_reflexion=image[1]) 
-            res = []
-            for I in points:
-                if I[0]:                                                 
-                    # I[0] vaut False si c'est un point fictif, qui ne se trouve pas sur la grille ou qui ne peut pas être tracé
-                    I2=intersection(image[0],I[0],image[1],grille)                      
-                    # On détermine l'intersection du nouveau point avec le point intermédiaire image
-                    if I2:
-                        # Si S est bien le premier point source, il se distingue en étant à l'intérieur, alors on trace le segment [SI2]
-                        if dedans(S,grille):                                    
-                            tracer(S,I2)
-                        # Le point S n'est pas le véritable point source mais une source fictive, on ajoute seulement la distance I,I2
-                        if I2!=I[0]:
-                            # On trace le rayon entre le nouveau point et le point intermédiaire 
-                            tracer(I2,I[0])   
-"""                                                       
+        return res                                                    
                 
 """
     Check if a point A is in a grid. Grid is described as [-x,x,y,-y]
 """
-def dedans(A,grille):
+def dedans(A:Point,grille:Polygon):
     xa,ya=A
     # Ici on suppose que la grille est de la forme (-abscisse,abscisse,ordonnée,-ordonnée) elle est rectangulaire
     if abs(round(xa))<=abs(grille[0]) and abs(round(ya))<=abs(grille[2]):                     
         return True
     return False
 
-def intersection(line1, line2):
-    xa,ya = line1[0]
-    xb,yb = line1[1]
-    xc,yc = line2[0]
-    xd,yd = line2[1]
-    # Check if each line is well defined 
-    if xa == xb and ya == yb:
-        raise ValueError('Line 1 is not defined by two different point, points are the same !')
-    elif xc == xd and yc == yd:
-        raise ValueError('Line 2 is not defined by two different point, points are the same !')
-    # Check if the two lines are parallel using cross product
-    elif (xb-xa)*(yd-yc) - (xd-xc)*(yb-ya) == 0:
-        return None 
-    else :
-        # This is the result of solving the system of equations of the intersection of two lines. This value exist because the cross product is not null here.
-        k = ((xb-xd)*(ya-yd) - (yb-yd)*(xa-xb))/((xc-xd)*(ya-yb) - (xa-xb)*(yc-yd))
-        return [k*xc + (1-k)*xd, k*yc + (1-k)*yd]
-
-
-
-def isBetween(departure,arrival,point):
-    if departure == arrival : 
-        raise ValueError("Departure and arrival are the same point !")
-    else :
-        xa,ya = departure
-        xb,yb = arrival
-        xc,yc = point
-        if xa != xb:
-            lbda = (xc-xb)/(xa-xb)
-            return 0 <= lbda <= 1 and yc == lbda*ya + (1-lbda)*yb
-        else : 
-            lbda = (yc-yb)/(ya-yb)
-            return 0 <= lbda <= 1 and xc == lbda*xa + (1-lbda)*xb
-
-def segmentInTheGrid(S,A,grille):
-    # D'abord on vérifie si le point est dans la grille auquel cas on renvoie S lui-meme
-    # on cherche l'intersection du segment [SA] avec le miroir "derniere_reflexion"
-    if S == A : 
-        raise ValueError("Departure and arrival are the same point !")
-    hauteur, cote = grille[2], grille[1]
-    xa,ya=A
-    xs,ys=S
-    # We define a line by two different points of this line
-    currentLine = []
-    linesOfGrid = [[0,cote], [cote,cote]], [[0, -cote], [-cote, -cote]], [[cote, 0], [cote,cote]], [[-cote, 0], [-cote, cote]]
-    droiteCoteHaut,droiteCoteBas,droiteCoteDroit,droiteCoteGauche = linesOfGrid 
-    # The list above may contain None Value
-    intersectionsWithGrid = list(map(lambda line : intersection(line, [S,A]), linesOfGrid))
-    # We filter NoneValues and intersections out of the grid
-    intersectionsInTheGrid = list(filter(lambda point : point!=None and dedans(point, grille) and isBetween(S, A, point), intersectionsWithGrid))
-    if dedans(S,grille) and dedans(A, grille):
-        return [S,A]
-    elif dedans(S,grille): # Then len(intersectionsInTheGrid)>0 :
-        return [S, intersectionsInTheGrid[0]]
-    elif dedans(A, grille) and len(intersectionsInTheGrid)>0 : 
-        return [intersectionsInTheGrid[0], A]
-    elif intersectionsInTheGrid: # Then len(intersectionsInTheGrid) == 2
-        return intersectionsInTheGrid
-    else:
-        return None
-
-
-
-"""
-    Returns the intersection of a segment between 2 edges with a miror. It's useful when one of the edges is behind the mirror. 
-
-def intersection(S,A,plan_reflexion,grille):
-    # D'abord on vérifie si le point est dans la grille auquel cas on renvoie S lui-meme
-    # on cherche l'intersection du segment [SA] avec le miroir "derniere_reflexion"
-    orientation,pos_ref=plan_reflexion
-    xa,ya=A
-    xs,ys=S
-    # On détermine l'équation de la droite (SA) : y=mx+b
-    m=(ys-ya)/(xs-xa)
-    b=ya-m*xa
-    # Si le miroir est horizontal, sa position est une ordonnée
-    if orientation == 'horizontal':                                         
-        yintersection=pos_ref
-        xintersection=(yintersection-b)/m
-    # Si le miroir est vertical, sa position est une abscisse
-    else:                                                                   
-        xintersection=pos_ref
-        yintersection=xintersection*m+b 
-    # Si le point d'intersection déterminé n'est pas sur la grille, il n'existe pas, on renvoie False
-    if dedans((xintersection,yintersection),grille):
-        drawPoint((xintersection,yintersection),'black')
-        return (xintersection,yintersection) 
-    return False     
-
-"""
-
-"""
-def intersection(S,A,grille):
-    xa,ya=A
-    xs,ys=S
-    cote = grille[1]
-    hauteur = grille[2]
-    # On détermine l'équation de la droite (SA) : y=mx+b
-    m=(ys-ya)/(xs-xa)
-    b=ya-m*xa
-    resultPoint =[]
-    # Si le miroir est horizontal, sa position est une ordonnée
-    if -hauteur<=ys<=hauteur:
-        if xs<xa:
-            resultPoint = [-cote,-m*cote+b]
-        else:
-            resultPoint = [cote,m*cote+b]
-    elif ys>hauteur:
-        resultPoint = [(hauteur-b)/m, hauteur]
-    else :
-        resultPoint = [(-hauteur-b)/m, -hauteur]
-    drawPoint(resultPoint,'black')
-    print(dedans(resultPoint,grille))
-    return resultPoint
-
-    if orientation == 'horizontal':                                         
-        yintersection=pos_ref
-        xintersection=(yintersection-b)/m
-    # Si le miroir est vertical, sa position est une abscisse
-    else:                                                                   
-        xintersection=pos_ref
-        yintersection=xintersection*m+b 
-    # Si le point d'intersection déterminé n'est pas sur la grille, il n'existe pas, on renvoie False
-    if dedans((xintersection,yintersection),grille):
-        drawPoint((xintersection,yintersection),'black')
-        return (xintersection,yintersection) 
-    return False     
-"""
-
 """ 
     Procedure initializing the window and drawing the initial grid.
 """
-def tracer_grille(grille):
-    x1,x2,y1,y2=grille
+def tracer_grille(grille:Polygon):
+    x1,x2,y1,y2=grille.vertices
     ADDITIONAL_SIZE = 300
     sizeOfScreen=abs(x1-x2) + ADDITIONAL_SIZE
     tu.setup(sizeOfScreen,sizeOfScreen)
@@ -240,7 +66,7 @@ def tracer_grille(grille):
 """
     Draws a link between two points
 """
-def tracer(P,I,color_fleche='black'):
+def tracer(P:Point,I:Point,color_fleche='black'):
     x1,y1=P
     x2,y2=I
     milieu=((x1+x2)/2,(y1+y2)/2)
@@ -257,13 +83,12 @@ def tracer(P,I,color_fleche='black'):
 """
     Draw a point of a specific color on a position.
 """
-def drawPoint(S,color):
+def drawPoint(S:Point,color:str):
     tu.up()
     tu.goto(S)
     tu.dot(None,color)
     
-
-def main(S,A,grille,nombre_de_reflexions=3):
+def main(S:Point,A:Point,grille:Polygon,nombre_de_reflexions=3):
     # Dans l'ordre, on trace la grille, place les points de départ et d'arrivés S et A, puis on trace les différents rayons de S à A sur la grille en n (ici n=2) réflexions.
     tracer_grille(grille)
     drawPoint(S,'blue')
@@ -274,21 +99,17 @@ def main(S,A,grille,nombre_de_reflexions=3):
     n=len(L)
     return [L[k][1]*2.6*(10**-4)/lightSpeed for k in range(n)],n
 
-
-
-
-
 def creneaux(t,periode=0.5,amplitude=1,dephasage=0):
     if ((t+dephasage)//periode)%2==1: return amplitude
     else : return 0
 
     
 # Caractéristiques de la simulation :
-cote=300
-grille=(-cote,cote,cote,-cote)
+COTE=300
+grille=Polygon(Point(-COTE, -COTE), Point(-COTE, COTE), Point(COTE, COTE), Point(COTE, -COTE))
 #S,A=Point((2*random()-1)*cote,(2*random()-1)*cote)  ,  PoinS,A=Point((2*random()-1)*cote,(2*random()-1)*cote)
 
-S,A = Point(-2*cote,0), Point(0,0)
+S,A = Point(-2*COTE,0), Point(0,0)
 
 #print(main(S,A,grille,1))
 segmentInTheGrid(S,A, grille)
